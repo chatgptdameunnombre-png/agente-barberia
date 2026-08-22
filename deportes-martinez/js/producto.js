5c0e402b54e21d35531326014322b7c09bfeab2f
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-22 17:48:55 -0600

    deportes-martinez: js/producto.js

--- deportes-martinez/js/producto.js
+++ deportes-martinez/js/producto.js
@@ -1,10 +1,10 @@
 import { db, MODO } from "./db.js?v=3";
 import { setProductos, initCart, enCarrito, addCart } from "./cart.js?v=3";
 import { ENVIO_DOMICILIO, PERSONALIZACION_PRECIO } from "./config.js?v=2";
-import { iniciarPago, iniciarTransferencia } from "./checkout.js?v=2";
+import { iniciarPago, iniciarTransferencia } from "./checkout.js?v=3";
 import { tieneTallas, tallasDe, stockDeTalla, stockTotal, precioTalla, precioDesde, preciosVarian, etiquetaStock } from "./tallas.js?v=1";
 import { onMayoreo, precioHTML, precioMay } from "./mayoreo.js?v=1";
-import { track, trackProducto, cerrarProducto } from "./track.js?v=3";
+import { track, trackProducto, cerrarProducto } from "./track.js?v=4";
 
 const $ = s => document.querySelector(s);
 const money = n => "$" + Number(n).toLocaleString("es-MX");
