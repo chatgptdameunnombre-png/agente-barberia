95b708991edb488735f23d267f85bae33fd36e0b
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-22 22:48:11 -0600

    deportes-martinez: js/asesor.js

--- deportes-martinez/js/asesor.js
+++ deportes-martinez/js/asesor.js
@@ -1,6 +1,6 @@
-import { ASESOR_WEBHOOK, NEGOCIO, firebaseConfig } from "./config.js?v=33";
-import { track } from "./track.js?v=33";
-import { tieneTallas, stockTotal, precioDesde, preciosVarian } from "./tallas.js?v=33";
+import { ASESOR_WEBHOOK, NEGOCIO, firebaseConfig } from "./config.js?v=34";
+import { track } from "./track.js?v=34";
+import { tieneTallas, stockTotal, precioDesde, preciosVarian } from "./tallas.js?v=34";
 
 const money = n => "$" + Number(n).toLocaleString("es-MX");
 const SID_KEY = "deportes-martinez_asesor_sid";
@@ -14,7 +14,7 @@ function setProductos(list) {
   if (alCargarProductos) alCargarProductos();
 }
 
-import("./db.js?v=33").then(m => { m.db.onProducts(list => setProductos(list)); }).catch(() => {});
+import("./db.js?v=34").then(m => { m.db.onProducts(list => setProductos(list)); }).catch(() => {});
 
 function fsVal(v) {
   const t = Object.keys(v)[0];
