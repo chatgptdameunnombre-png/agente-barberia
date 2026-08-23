30ca905554f527ed00844b8244ef7277a929b39a
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-22 18:31:34 -0600

    deportes-martinez: js/store.js

--- deportes-martinez/js/store.js
+++ deportes-martinez/js/store.js
@@ -1,8 +1,8 @@
-import { db, MODO } from "./db.js?v=3";
+import { db, MODO } from "./db.js?v=4";
 import { setProductos, initCart, enCarrito } from "./cart.js?v=3";
 import { tieneTallas, stockTotal, precioDesde, preciosVarian, etiquetaStock, tallasDisponibles } from "./tallas.js?v=1";
 import { onMayoreo, precioHTML } from "./mayoreo.js?v=1";
-import { track } from "./track.js?v=5";
+import { track } from "./track.js?v=6";
 
 const $ = s => document.querySelector(s);
 const CAT = document.querySelector("#catalogo")?.dataset.categoria || null;
