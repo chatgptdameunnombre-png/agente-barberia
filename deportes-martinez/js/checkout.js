f201cee26036bfac752b1ce0adf701697dc4fea6
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-22 19:37:46 -0600

    deportes-martinez: js/checkout.js

--- deportes-martinez/js/checkout.js
+++ deportes-martinez/js/checkout.js
@@ -1,7 +1,7 @@
-import { COBRO_WEBHOOK, ENVIO_DOMICILIO, WHATSAPP_NUMERO } from "./config.js?v=30";
-import { db } from "./db.js?v=30";
-import { esMayorista as soyMayorista } from "./mayoreo.js?v=30";
-import { track } from "./track.js?v=30";
+import { COBRO_WEBHOOK, ENVIO_DOMICILIO, WHATSAPP_NUMERO } from "./config.js?v=31";
+import { db } from "./db.js?v=31";
+import { esMayorista as soyMayorista } from "./mayoreo.js?v=31";
+import { track } from "./track.js?v=31";
 
 const money = n => "$" + Number(n).toLocaleString("es-MX");
 
