5163b15c5bee280b7f3fdae901579e317f023eab
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-22 20:17:26 -0600

    deportes-martinez: js/db.js

--- deportes-martinez/js/db.js
+++ deportes-martinez/js/db.js
@@ -1,5 +1,5 @@
-import { firebaseConfig, usaFirebase } from "./config.js?v=31";
-import { PRODUCTOS_SEED } from "./seed.js?v=31";
+import { firebaseConfig, usaFirebase } from "./config.js?v=32";
+import { PRODUCTOS_SEED } from "./seed.js?v=32";
 
 const LS_KEY = "dm_productos";
 const LS_AUTH = "dm_auth";
