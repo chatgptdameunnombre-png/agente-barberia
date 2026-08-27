72af3e9b894e3a4494682034161e25068490b294
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-27 16:12:49 -0600

    deportes-martinez: js/track.js

--- deportes-martinez/js/track.js
+++ deportes-martinez/js/track.js
@@ -1,5 +1,5 @@
-import { firebaseConfig, usaFirebase } from "./config.js?v=49";
-import { permiteMedicion } from "./cookies.js?v=49";
+import { firebaseConfig, usaFirebase } from "./config.js?v=50";
+import { permiteMedicion } from "./cookies.js?v=50";
 
 const KEY = firebaseConfig.apiKey;
 const PROJ = firebaseConfig.projectId;
