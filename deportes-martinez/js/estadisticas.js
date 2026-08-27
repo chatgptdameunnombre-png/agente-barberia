ddf07a15e85370e5cb1897c70035cfacba8e9dc7
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-27 12:35:30 -0600

    deportes-martinez: js/estadisticas.js

--- deportes-martinez/js/estadisticas.js
+++ deportes-martinez/js/estadisticas.js
@@ -1,4 +1,4 @@
-import { firebaseConfig } from "./config.js?v=42";
+import { firebaseConfig } from "./config.js?v=43";
 
 const $ = s => document.querySelector(s);
 const PROJ = firebaseConfig.projectId;
