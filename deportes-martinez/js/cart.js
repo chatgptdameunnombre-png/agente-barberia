1ca61a931911fbe3526d1fc11ea635fe63047ba3
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-27 17:00:33 -0600

    deportes-martinez: js/cart.js

--- deportes-martinez/js/cart.js
+++ deportes-martinez/js/cart.js
@@ -1,8 +1,8 @@
-import { ENVIO_DOMICILIO, PERSONALIZACION_PRECIO } from "./config.js?v=55";
-import { iniciarPago, iniciarTransferencia } from "./checkout.js?v=55";
-import { tieneTallas, stockDeTalla, stockTotal, precioTalla } from "./tallas.js?v=55";
-import { onMayoreo, precioHTML, precioMay } from "./mayoreo.js?v=55";
-import { track, marcarProducto } from "./track.js?v=55";
+import { ENVIO_DOMICILIO, PERSONALIZACION_PRECIO } from "./config.js?v=56";
+import { iniciarPago, iniciarTransferencia } from "./checkout.js?v=56";
+import { tieneTallas, stockDeTalla, stockTotal, precioTalla } from "./tallas.js?v=56";
+import { onMayoreo, precioHTML, precioMay } from "./mayoreo.js?v=56";
+import { track, marcarProducto } from "./track.js?v=56";
 
 const CART_KEY = "dm_cart";
 const $ = s => document.querySelector(s);
