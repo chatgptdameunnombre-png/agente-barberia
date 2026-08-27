5ec567df13806c354315d6956e8c635e93a5435d
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-27 16:10:02 -0600

    deportes-martinez: js/cuenta.js

--- deportes-martinez/js/cuenta.js
+++ deportes-martinez/js/cuenta.js
@@ -1,4 +1,4 @@
-import { db } from "./db.js?v=48";
+import { db } from "./db.js?v=49";
 
 const OWNER_EMAILS = ["admindeportesmartinez@gmail.com"];
 const esDueno = u => !!u && OWNER_EMAILS.includes((u.email || "").toLowerCase());
