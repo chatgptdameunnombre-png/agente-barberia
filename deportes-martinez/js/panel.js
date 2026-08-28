58b4d03213bbf1ab8e0f56abc3836dd76596dce8
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-28 17:29:08 -0600

    deportes-martinez: js/panel.js

--- deportes-martinez/js/panel.js
+++ deportes-martinez/js/panel.js
@@ -1,6 +1,6 @@
-import { db } from "./db.js?v=67";
-import { pintarEstadisticas } from "./estadisticas.js?v=67";
-import "./panel-nav.js?v=67";
+import { db } from "./db.js?v=68";
+import { pintarEstadisticas } from "./estadisticas.js?v=68";
+import "./panel-nav.js?v=68";
 
 const $ = s => document.querySelector(s);
 const money = n => "$" + Number(n).toLocaleString("es-MX");
