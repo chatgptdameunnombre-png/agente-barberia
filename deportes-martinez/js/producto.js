f7ce5ab7d5767dee1616edf07e030b3078018517
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-27 18:03:32 -0600

    deportes-martinez: js/producto.js

--- deportes-martinez/js/producto.js
+++ deportes-martinez/js/producto.js
@@ -1,10 +1,10 @@
-import { db, MODO } from "./db.js?v=57";
-import { setProductos, initCart, enCarrito, addCart } from "./cart.js?v=57";
-import { ENVIO_DOMICILIO, PERSONALIZACION_PRECIO } from "./config.js?v=57";
-import { iniciarPago, iniciarTransferencia } from "./checkout.js?v=57";
-import { tieneTallas, tallasDe, stockDeTalla, stockTotal, precioTalla, precioDesde, preciosVarian, etiquetaStock } from "./tallas.js?v=57";
-import { onMayoreo, precioHTML, precioMay } from "./mayoreo.js?v=57";
-import { track, trackProducto, cerrarProducto } from "./track.js?v=57";
+import { db, MODO } from "./db.js?v=58";
+import { setProductos, initCart, enCarrito, addCart } from "./cart.js?v=58";
+import { ENVIO_DOMICILIO, PERSONALIZACION_PRECIO } from "./config.js?v=58";
+import { iniciarPago, iniciarTransferencia } from "./checkout.js?v=58";
+import { tieneTallas, tallasDe, stockDeTalla, stockTotal, precioTalla, precioDesde, preciosVarian, etiquetaStock } from "./tallas.js?v=58";
+import { onMayoreo, precioHTML, precioMay } from "./mayoreo.js?v=58";
+import { track, trackProducto, cerrarProducto } from "./track.js?v=58";
 
 const $ = s => document.querySelector(s);
 const money = n => "$" + Number(n).toLocaleString("es-MX");
