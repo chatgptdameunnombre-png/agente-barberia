a3f4770cf6252b8b90dba48609178f3e9e76e4a8
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-28 10:34:46 -0600

    deportes-martinez: js/store.js

--- deportes-martinez/js/store.js
+++ deportes-martinez/js/store.js
@@ -1,8 +1,8 @@
-import { db, MODO } from "./db.js?v=65";
-import { setProductos, initCart, enCarrito } from "./cart.js?v=65";
-import { tieneTallas, stockTotal, precioDesde, preciosVarian, etiquetaStock, tallasDisponibles } from "./tallas.js?v=65";
-import { onMayoreo, precioHTML } from "./mayoreo.js?v=65";
-import { track } from "./track.js?v=65";
+import { db, MODO } from "./db.js?v=66";
+import { setProductos, initCart, enCarrito } from "./cart.js?v=66";
+import { tieneTallas, stockTotal, precioDesde, preciosVarian, etiquetaStock, tallasDisponibles } from "./tallas.js?v=66";
+import { onMayoreo, precioHTML } from "./mayoreo.js?v=66";
+import { track } from "./track.js?v=66";
 
 const $ = s => document.querySelector(s);
 const CAT = document.querySelector("#catalogo")?.dataset.categoria || null;
