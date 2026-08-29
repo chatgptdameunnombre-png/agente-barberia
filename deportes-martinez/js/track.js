7ba53957b78ba8e673bb53312407d7656ae74b53
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-28 19:09:53 -0600

    deportes-martinez: js/track.js

--- deportes-martinez/js/track.js
+++ deportes-martinez/js/track.js
@@ -1,5 +1,5 @@
-import { firebaseConfig, usaFirebase } from "./config.js?v=69";
-import { permiteMedicion } from "./cookies.js?v=69";
+import { firebaseConfig, usaFirebase } from "./config.js?v=70";
+import { permiteMedicion } from "./cookies.js?v=70";
 
 const KEY = firebaseConfig.apiKey;
 const PROJ = firebaseConfig.projectId;
