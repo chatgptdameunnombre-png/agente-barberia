7462650f511bc31a364ec7ffe50286b1e1ab015c
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-09-14 18:35:43 -0600

    deportes-martinez: js/sugerencias.js

--- deportes-martinez/js/sugerencias.js
+++ deportes-martinez/js/sugerencias.js
@@ -3,7 +3,7 @@
    Escribe directo a Firestore: la regla deja crear a cualquiera pero solo el
    dueño puede leer, y ahí mismo se valida el tope de 200 caracteres. */
 
-import { firebaseConfig } from "./config.js?v=78";
+import { firebaseConfig } from "./config.js?v=79";
 
 const PROJ = firebaseConfig.projectId;
 const KEY = firebaseConfig.apiKey;
